e606f8e71bdb0c2c42ccdff3e8649e30b51d8380
Esmail Mohamed Esmail Elkot <147884500+esmailelkot74@users.noreply.github.com>
2025-03-31 23:31:13 +0200

    Update HomeController.cs

--- EcommerceDEPI/Controllers/HomeController.cs
+++ EcommerceDEPI/Controllers/HomeController.cs
@@ -1,22 +1,33 @@
 using System.Diagnostics;
 using EcommerceApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
-namespace EcommerceDEPI.Controllers
+namespace EcommerceDEPI.Models
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        
-        public IActionResult Index()
+        [Authorize]
+        public IActionResult Index(string search)
         {
-             return View();
+            var products = _context.Products
+                .Include(p => p.Category)
+                .Where(p => string.IsNullOrEmpty(search) ||
+                            p.Name.Contains(search) ||
+                            p.Description.Contains(search))
+                .ToList();
+
+            return View(products);
         }
 
         public IActionResult Privacy()
